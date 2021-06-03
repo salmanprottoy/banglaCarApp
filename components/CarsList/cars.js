d3cbea62c61af6d4b1f48a03d64c7b6cc992d3b5
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-06-03 23:13:12 +0600

    update

--- components/CarsList/cars.js
+++ components/CarsList/cars.js
@@ -4,18 +4,21 @@ export default [
     tagline: "Starting at ৳3000000",
     image: require("../../assets/images/ModelS.jpeg"),
   },
+  
   {
     name: "Model 3",
     tagline: "Order Online for",
     taglineCTA: "Touchless Delivery",
     image: require("../../assets/images/Model3.jpeg"),
   },
+
   {
     name: "Model X",
     tagline: "Order Online for",
     taglineCTA: "Touchless Delivery",
     image: require("../../assets/images/ModelX.jpeg"),
   },
+
   {
     name: "Model Y",
     tagline: "Order Online for",
