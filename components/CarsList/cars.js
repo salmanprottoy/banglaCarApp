70b5e82251cbd9b03b328310c98d578a2b346d15
Abdur rahman <abdur.rahman200301@gmail.com>
2021-06-04 11:50:41 +0600

    style change

--- components/CarsList/cars.js
+++ components/CarsList/cars.js
@@ -1,14 +1,15 @@
 export default [
   {
-    name: "Version Pro max",
-    tagline: "Starting at ৳7000000",
-    image: require("../../assets/images/3.jpg"),
-  },
-  {
     name: "Version v7",
     tagline: "Starting at ৳6000000",
     image: require("../../assets/images/ModelS.jpeg"),
   },
+  {
+    name: "Version Pro max",
+    tagline: "Starting at ৳7000000",
+    image: require("../../assets/images/3.jpg"),
+  },
+
   {
     name: "Version v5",
     tagline: "Starting at ৳3000000",
